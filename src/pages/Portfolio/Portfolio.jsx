12ff895139da6827331313dda6a517f0740c4f6a
Lara Oliveira <laraof.dev@gmail.com>
2022-07-14 21:49:45 -0300

    Adiciona página Portfolio

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,13 +1,59 @@
 import Header from '../../componentes/Header/Header'
 import project from '../../assets/project-complete.svg'
+import projects from './projects'
+import { BsArrowReturnRight } from 'react-icons/bs'
+import "./portfolio.css"
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 function Portfolio() {
+    const [ repos, setRepos ] = useState([])
+    const baseURL = 'https://api.github.com/users/laraof/repos'
+
+    useEffect(() => {
+      axios
+      .get(baseURL)
+      .then((response) => setRepos(response.data))
+    }, [])
+  
     return (
-        <Header 
-        image={project} 
-        description="ilustração projects"
-        >Meus Projetos
-        </Header>
+        <>
+            <Header 
+            image={project} 
+            description="ilustração projects"
+            >Meus Projetos
+            </Header>
+            <div className="cartao-container">
+              {projects.map(infos => {
+          return (
+            <div className="cartao" key={infos.id}>
+              <h3 className="skills">{infos.name}</h3>
+              <img src={infos.poster} alt={infos.name} />
+              <a href={infos.link} className="enter-repo" target="_blank">
+                <BsArrowReturnRight size={16} color="#111"/>
+              </a>
+            </div>
+          
+          )
+          })}
+          </div>
+          <h2 className='titulo'>Outros Projetos no Meu Github</h2>
+          <div className="card-container">
+            {
+              repos.map(repo => {
+                return(
+                  <div className="card" key={repo.id}>
+                    <h3>{repo.name}</h3>
+                    <p>{repo.description}</p>
+                    <a href={repo.html_url} className="enter-repo" target="_blank">
+                <BsArrowReturnRight size={16} color="#fff"/>
+              </a>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </>
     )
 }
 
---
+++ src/pages/Portfolio/projects.js
@@ -0,0 +1,33 @@
+import lc from "../../assets/lc.gif"
+import bts from "../../assets/bts.gif"
+import elis from "../../assets/elis.gif"
+import digivice from "../../assets/digivice.gif"
+
+const projects = [
+    {
+        id: 1,
+        name: "L.C. Arquitetura e Interiores",
+        poster: lc,
+        link: "https://github.com/laraof/On17-TodasEmTech-JavascriptV"
+    },
+    {
+        id: 2,
+        name: "We Are Bulletproof - BTS",
+        poster: bts,
+        link: "https://github.com/laraof/On17-TodasEmTech-JavascriptV"
+    },
+    {
+        id: 3,
+        name: "M de Maravilhosa - Elis Regina",
+        poster: elis,
+        link: "https://github.com/laraof/On17-TodasEmTech-JavascriptV"
+    },
+    {
+        id: 4,
+        name: "Digivice",
+        poster: digivice,
+        link: "https://github.com/laraof/On17-TodasEmTech-JavascriptV"
+    },
+  ]
+
+  export default projects
